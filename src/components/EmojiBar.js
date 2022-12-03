198a10fdc8a79d397430dd418435c34bd0599b32
Alexey Ilyukhov <l1vac3@gmail.com>
2022-12-03 23:02:28 +0300

    Animations kinda work

--- src/components/EmojiBar.js
+++ src/components/EmojiBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import EmojiSpan from './EmojiSpan';
 import Favicon from 'react-favicon'
-import { SwitchTransition } from 'react-transition-group'
+import FadeReplace from './FadeReplace';
 
 class EmojiBar extends React.Component {
   constructor(props) {
@@ -20,10 +20,10 @@ class EmojiBar extends React.Component {
     }
 
     return (
-      <div>
+      <FadeReplace animationKey={this.props.index}>
         <Favicon url={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${this.props.value[0].emoji}</text></svg>`} />
         {spans}
-      </div>
+      </FadeReplace>
     )
   }
 }
--- src/components/EmojiSpan.js
+++ src/components/EmojiSpan.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './EmojiSpan.css'
+import FadeReplace from './FadeReplace';
 
 class EmojiSpan extends React.Component {
   constructor(props) {
@@ -9,15 +11,19 @@ class EmojiSpan extends React.Component {
     console.log(this.props)
     if (this.props.show) {
       return (
-        <span onClick={this.props.handle_click}>
-          {this.props.text}
-        </span>
+        <FadeReplace animationKey='0'>
+          <span className='emoji' onClick={this.props.handle_click}>
+            {this.props.text}
+          </span>
+        </FadeReplace>
       )
   } else {
       return (
-        <span onClick={this.props.handle_click}>
-          {this.props.emoji}
-        </span>
+        <FadeReplace animationKey='1'>
+          <span className='emoji' onClick={this.props.handle_click}>
+            {this.props.emoji}
+          </span>
+        </FadeReplace>
       )
     }
   }
